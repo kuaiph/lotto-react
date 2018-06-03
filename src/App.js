c2f1fb32ab44af8ee14163db4216e9be0dbb1400
Tony Thompson <penguinprofession@icloud.com>
2018-06-03 14:46:58 -0400

    refactored to display contract after component mounts

--- src/App.js
+++ src/App.js
@@ -2,21 +2,25 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import web3 from './web3';
+import lotto from './lotto';
 
 
 class App extends Component {
+  constructor(props){
+    super(props)
+    this.state = {manager:''};
+  }
+  async componentDidMount(){
+    const manager = await lotto.methods.manager().call();
+    this.setState({manager});
+  }
   render() {
     // console.log(web3.version);
     web3.eth.getAccounts().then(console.log);
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <h2>Lotto Contract</h2>
+        <p>This contract is managed by {this.state.manager}</p>
       </div>
     );
   }
